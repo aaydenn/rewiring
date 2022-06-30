c0dd21f78abbccc2d8a7ba18d32d6a4f60d214eb
Atakan Ayden <atakanayden@gmail.com>
2022-07-01 02:41:50 +0300

    add differential eigen rank code and table

---
+++ code/15-differential_eigen_rank.R
@@ -0,0 +1,40 @@
+tmp <- theta.centrality$Blood.AL
+
+tmp |> mutate(rank=1:nrow(tmp))
+
+theta.centrality.ranked <- lapply(theta.centrality, function(x) x |> mutate(eigen.rank=1:nrow(x)) |> select(miRNA,eigen.rank))
+
+#' ------------------------------------------------------------------
+#' condition dependent differential eigen centralitty ranks of miRNAs
+
+
+#' Blood
+
+diff.rank.Blood.CCR.AL <- inner_join(theta.centrality.ranked$Blood.AL,theta.centrality.ranked$Blood.CCR,by="miRNA") |> 
+  mutate(d.rank = eigen.rank.x - eigen.rank.y) |> 
+  mutate(z.rank = (d.rank- mean(d.rank))/sd(d.rank)) |> filter(abs(z.rank)>1)
+
+diff.rank.Blood.ICR.AL <- inner_join(theta.centrality.ranked$Blood.AL,theta.centrality.ranked$Blood.ICR,by="miRNA") |> 
+  mutate(d.rank = eigen.rank.x - eigen.rank.y) |> 
+  mutate(z.rank = (d.rank- mean(d.rank))/sd(d.rank)) |> filter(abs(z.rank)>1)
+
+
+#' MFP
+ 
+diff.rank.MFP.CCR.AL <- inner_join(theta.centrality.ranked$MFP.AL,theta.centrality.ranked$MFP.CCR,by="miRNA") |> 
+  mutate(d.rank = eigen.rank.x - eigen.rank.y) |> 
+  mutate(z.rank = (d.rank- mean(d.rank))/sd(d.rank)) |> filter(abs(z.rank)>1)
+
+diff.rank.MFP.ICR.AL <- inner_join(theta.centrality.ranked$MFP.AL,theta.centrality.ranked$MFP.ICR,by="miRNA") |> 
+  mutate(d.rank = eigen.rank.x - eigen.rank.y) |> 
+  mutate(z.rank = (d.rank- mean(d.rank))/sd(d.rank)) |> filter(abs(z.rank)>1)
+
+diff.rank.eigen <- list(
+    diff.rank.Blood.CCR.AL = diff.rank.Blood.CCR.AL,
+    diff.rank.Blood.ICR.AL = diff.rank.Blood.ICR.AL,
+    diff.rank.MFP.CCR.AL = diff.rank.MFP.CCR.AL,
+    diff.rank.MFP.ICR.AL = diff.rank.MFP.ICR.AL
+  )
+
+
+openxlsx::write.xlsx(diff.rank.eigen, "tables/diff.eigen.rank.xlsx")
--- figures/04-distribution.R
+++ figures/04-distribution.R
@@ -1,188 +1,60 @@
 library(igraph)
-load("Data/d.fgl.Rdata") # first level mirna-mirna (GLASSO) ####
+
+load("Data/fgl.Rdata") # first level mirna-mirna (GLASSO) ####
 load("Data/targets.Rdata") # second level mirna-gene (mirnatap) ####
-# choose only connected graphs
-deltagraphs <- list(CCR.Blood = d.fgl$CCR$Blood$d.fgl$Delta.graph.connected, 
-                    ICR.Blood = d.fgl$ICR$Blood$d.fgl$Delta.graph.connected,
-                    CCR.MFP = d.fgl$CCR$MFP$d.fgl$Delta.graph.connected,
-                    ICR.MFP = d.fgl$ICR$MFP$d.fgl$Delta.graph.connected)
-# rename
-targets.unique |> mutate(mirna = str_remove(mirna, "mmu-")) -> targets
 
-# make graphs targeted mutually
-mir_graph <- lapply(deltagraphs, as.directed, mode = "mutual")
+#' -------------------------------
+# degree distribution for each delta
 
-# keep only target pointed by these miRNA
-mirna_targets_graph <-
-  lapply(lapply(mir_graph, function(x)
-    subset(targets, mirna %in% V(x)$name)), graph_from_data_frame)
+delta.degree.dist <- lapply(delta.graphs, function(x) {
+  x |> 
+    degree() |> 
+    table() |> 
+    as.data.frame() |> 
+    mutate(Var1 = as.numeric(Var1))
+})
 
-# degree distribution for each DCE
-degree_dist <- function(net1, net2, netid, title = names(net1)) {
-  # count the frequencies of each degree
-  # convert the first column to numbers
-  deg.hist1 <-
-    as.data.frame(table(degree(net1))) |>
-    mutate(Var1 = as.numeric(Var1), id = netid[1])
-  
-  deg.hist2 <-
-    as.data.frame(table(degree(net2))) |>
-    mutate(Var1 = as.numeric(Var1), id = netid[2])
-  
-  deg.hist <- rbind(deg.hist1, deg.hist2)
-  
-  #plot the degree distribution
-  ggplot(deg.hist, aes(x = Var1, y = Freq)) +
-    geom_point(aes(color = id)) +
-    geom_line(aes(color = id)) +
-    scale_x_continuous("Degree k",
-                       trans = "log10") +
-    scale_y_continuous("Frequency P(k)",
-                       trans = "log10") +
-    scale_color_manual("Type", values = c("orange", "purple")) +
-    labs(title = title, subtitle = "Degree Distribution (log-log)") + 
-    theme_bw() + 
-    theme(legend.position = c(0.7, 0.9))
-}
-
-ggsave(
-  filename = "Results/degdist_d.CCR.Blood.png",
-  degree_dist(mirna_targets_graph$CCR.Blood, 
-              deltagraphs$CCR.Blood, 
-              c("miRNA--target", "miRNA--miRNA"), 
-              "CCR vs. AL (Blood)"),
-  width = 7, height = 7, units = "in", dpi = 320
-)
-ggsave(
-  filename = "Results/degdist_d.ICR.Blood.png",
-  degree_dist(mirna_targets_graph$ICR.Blood, 
-              deltagraphs$ICR.Blood, 
-              c("miRNA--target", "miRNA--miRNA"), 
-              "ICR vs. AL (Blood)"),
-  width = 7, height = 7, units = "in", dpi = 320
-)
-ggsave(
-  filename = "Results/degdist_d.CCR.MFP.png",
-  degree_dist(mirna_targets_graph$CCR.MFP, 
-              deltagraphs$CCR.MFP, 
-              c("miRNA--target", "miRNA--miRNA"), 
-              "CCR vs. AL (MFP)"),
-  width = 7, height = 7, units = "in", dpi = 320
-)
-ggsave(
-  filename = "Results/degdist_d.ICR.MFP.png",
-  degree_dist(mirna_targets_graph$ICR.MFP, 
-              deltagraphs$ICR.MFP, 
-              c("miRNA--target", "miRNA--miRNA"), 
-              "ICR vs. AL (MFP)"),
-  width = 7, height = 7, units = "in", dpi = 320
+blood <- rbind(
+  data.frame(delta.degree.dist$D.Blood.CCR.AL, `Network ID` = "D.Blood.CCR.AL"),
+  data.frame(delta.degree.dist$D.Blood.ICR.AL, `Network ID` = "D.Blood.ICR.AL")
 )
 
-path_dist <- function(net, title) {
-  dist <- as.data.frame(table(distances(net)))
-  dist[, 1] <- as.numeric(dist[, 1])
-  # omit zero length
-  dist <- dist[-c(1),]
-  # omit infinite length
-  dist <- dist[-c(nrow(dist)),]
-  ggplot(data = dist, aes(x = Var1, y = log(Freq))) + 
-    geom_bar(stat = "identity") +
-    labs(title = title, subtitle = "Shortest Path Length Distribution (log)") +
-    xlab("Length of shortest path") + 
-    ylab("log(Frequency)") +
-    theme_bw()
-}
+p1 <- ggplot(blood, aes(Var1, log10(Freq), colour=Network.ID)) + 
+  geom_point()+
+  geom_smooth(se=FALSE)+
+  scale_color_manual(values=c("salmon2", "royalblue2"))+
+  geom_ribbon(stat = "smooth",
+              method = "loess",
+              se = TRUE,
+              alpha = 0.1, # or, use fill = NA
+              colour = "black",
+              linetype = "dotted")+
+  xlab("Degree(k)") + 
+  ylab("log(Frequency)") +
+  theme_bw()+
+  theme(legend.position = c(0.9, 0.9), legend.justification = c(1,1), legend.background = element_blank())+
+  theme(text=element_text(family="Times", size = 18));p1
 
-ggsave(
-  filename = "Results/degdist_d.CCR.Blood.png",
-  path_dist(mirna_targets_graph$CCR.Blood, "CCR vs. AL (Blood)"),
-  width = 6
-)
-ggsave(
-  filename = "Results/degdist_d.ICR.Blood.png",
-  path_dist(mirna_targets_graph$ICR.Blood, "ICR vs. AL (Blood)"),
-  width = 6
-)
-ggsave(
-  filename = "Results/degdist_d.CCR.MFP.png",
-  path_dist(mirna_targets_graph$CCR.MFP, "CCR vs. AL (MFP)"),
-  width = 6
-)
-ggsave(
-  filename = "Results/degdist_d.ICR.MFP.png",
-  path_dist(mirna_targets_graph$ICR.MFP, "ICR vs. AL (MFP)"),
-  width = 6
+mfp <- rbind(
+  data.frame(delta.degree.dist$D.MFP.CCR.AL, `Network ID` = "D.MFP.CCR.AL"),
+  data.frame(delta.degree.dist$D.MFP.ICR.AL, `Network ID` = "D.MFP.ICR.AL")
 )
 
-mir2gene <- function(net, title) {
-  sub <- targets |> subset(mirna %in% V(net)$name)
-  df <- as.data.frame(table(sub$mirna))
-  
-  ggplot(df, aes(x = Var1, y = Freq)) +
-    geom_bar(stat = "identity") + 
-    scale_color_manual("Type", values = "brown") +
-    labs(title = title) + 
-    theme_bw() + 
-    theme(legend.position = c(0.9, 0.9))
-}
+p2 <- ggplot(mfp, aes(Var1, log10(Freq), colour=Network.ID)) + 
+  geom_point()+
+  geom_smooth(se=FALSE)+
+  scale_color_manual(values=c("salmon2", "royalblue2"))+
+  geom_ribbon(stat = "smooth",
+              method = "loess",
+              se = TRUE,
+              alpha = 0.1, # or, use fill = NA
+              colour = "black",
+              linetype = "dotted")+
+  xlab("Degree(k)") + 
+  ylab("log(Frequency)") +
+  theme_bw()+
+  theme(legend.position = c(0.9, 0.9), legend.justification = c(1,1), legend.background = element_blank())+
+  theme(text=element_text(family="Times", size = 18));p2
 
-mir2gene(mirna_targets_graph$CCR.Blood, "ccr")
-
-degree_dist_hist <- function(net1, net2, netid, title = names(net1)) {
-  # count the frequencies of each degree
-  # convert the first column to numbers
-  deg.hist1 <-
-    as.data.frame(table(degree(net1))) |>
-    mutate(Var1 = as.numeric(Var1), id = netid[1])
-  
-  deg.hist2 <-
-    as.data.frame(table(degree(net2))) |>
-    mutate(Var1 = as.numeric(Var1), id = netid[2])
-  
-  deg.hist <- rbind(deg.hist1, deg.hist2)
-  
-  #plot the degree distribution
-  ggplot(deg.hist, aes(x = Var1, y = log(Freq))) +
-    geom_col(aes(color = id, fill = id), 
-             position = position_dodge(0.8), 
-             width = 0.7) +
-    labs(title = title, 
-         subtitle = "Degree Distribution (log-log)",
-         fill = "Interaction") + 
-    theme_bw()
-}
-
-degree_dist_hist(mirna_targets_graph$CCR.Blood, 
-            deltagraphs$CCR.Blood, 
-            c("miRNA--target", "miRNA--miRNA"), 
-            "CCR vs. AL (Blood)")
-
-degree_dist_hist(mirna_targets_graph$ICR.Blood, 
-                 deltagraphs$ICR.Blood, 
-                 c("miRNA--target", "miRNA--miRNA"), 
-                 "ICR vs. AL (Blood)")
-
-#####
-
-deg.hist1 <-
-  
-
-deg.hist2 <-
-  
-
-deg.hist <- rbind(as.data.frame(table(degree(deltagraphs$CCR.Blood))) |>
-                    mutate(Var1 = as.numeric(Var1), id = "miRNA--miRNA"), 
-                  as.data.frame(table(degree(mirna_targets_graph$CCR.Blood))) |>
-                    mutate(Var1 = as.numeric(Var1), id = "miRNA--target"))
-
-ggplot(deg.hist, aes(x = Var1, y = Freq)) +
-  geom_point(aes(color = id)) +
-  geom_line(aes(color = id)) +
-  scale_x_continuous("Degree k",
-                     trans = "log10") +
-  scale_y_continuous("Frequency P(k)",
-                     trans = "log10") +
-  scale_color_manual("Type", values = c("orange", "purple")) +
-  labs(title = title, subtitle = "Degree Distribution (log-log)") + 
-  theme_bw() + 
-  theme(legend.position = c(0.7, 0.9))
+ggsave("figures/delta.degree.dist.blood.png", p1, width = 8, height = 8, units = "in", dpi = 300)
+ggsave("figures/delta.degree.dist.mfp.png",   p2, width = 8, height = 8, units = "in", dpi = 300)
